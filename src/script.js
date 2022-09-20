d61800c897b9fbcb972eee8f0ad272706226ab3e
Frederick-Acquah <Frederickacquah.789@gmail.com>
2022-09-20 09:47:58 -0200

    modification and addition of files

--- src/script.js
+++ src/script.js
@@ -1,39 +1,39 @@
-// "use strict";
+"use strict";
 
-// let number1 = document.querySelector(".firstCounter");
-// let number2 = document.querySelector(".secondCounter");
-// let number3 = document.querySelector(".lastCounter");
+let number1 = document.querySelector(".firstCounter");
+let number2 = document.querySelector(".secondCounter");
+let number3 = document.querySelector(".lastCounter");
 
-// let count = 1;
+let count = 1;
 
-// //graduates count
+//graduates count
 
-// let countGraduates = () => {
-//     setInterval(() => {
-//         if(count <= 749){
-//             count++
-//             number1.innerHTML = count + " +";
-//         }
-//     }, 5)
-// }
+let countGraduates = () => {
+    setInterval(() => {
+        if(count <= 749){
+            count++
+            number1.innerHTML = count + " +";
+        }
+    }, 5)
+}
 
-// let countCompanies = () => {
-//     setInterval(() => {
-//        if(count <= 300){
-//         count ++;
-//         number2.innerHTML = count + "+";
-//        }
-//     }, 5)
-// }
+let countCompanies = () => {
+    setInterval(() => {
+       if(count <= 300){
+        count ++;
+        number2.innerHTML = count + "+";
+       }
+    }, 5)
+}
 
-// let countMentors = () => {
-//     setInterval(() => {
-//       if(count <= 500){
-//         count ++;
-//         number3.innerHTML = count + " +";
-//       }
-//     }, 1)
-// }
-// countGraduates();
-// countCompanies();
-// countMentors();
+let countMentors = () => {
+    setInterval(() => {
+      if(count <= 500){
+        count ++;
+        number3.innerHTML = count + " +";
+      }
+    }, 1)
+}
+countGraduates();
+countCompanies();
+countMentors();
